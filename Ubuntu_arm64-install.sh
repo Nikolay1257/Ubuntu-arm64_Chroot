9beefd5190ed3845ee3ed466b054bbe20fe05abf
Nikolay1257 <otraskinnikolaj@gmail.com>
2024-02-07 19:23:49 +0300

    fix eth v2

--- Ubuntu_arm64-install.sh
+++ Ubuntu_arm64-install.sh
@@ -6,7 +6,7 @@ cp ubuntu-base-22.04-base-arm64.tar.gz /data/local/Ubuntu/
 cd /data/local/Ubuntu/
 tar -xzvf /data/local/Ubuntu/ubuntu-base-22.04-base-arm64.tar.gz
 rm /data/local/Ubuntu/ubuntu-base-22.04-base-arm64.tar.gz
-cp /data/data/com.termux/files/home/Ubuntu-arm64_Chroot/main.sh /data/local/Ubuntu/
+cp /data/data/com.termux/files/home/Ubuntu-arm64_Chroot/eth.sh /data/local/Ubuntu/
 
 mount proc -t proc ./proc
 mount sys -t sysfs ./sys
